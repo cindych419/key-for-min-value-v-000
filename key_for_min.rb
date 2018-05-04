ac758e183bfda7e02b5378c5b891a09624b5b145
cindych419@gmail.com <cindych419@gmail.com>
2018-05-04 00:33:07 +0000

    automatically backed up by learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -1,6 +1,4 @@
 require 'pry'
-# prereqs: iterators, hashes, conditional logic
-# Given a hash with numeric values, return the key for the smallest value
 
 def key_for_min_value(name_hash)
 lowest_value= nil
@@ -12,6 +10,12 @@ name_hash.collect do |name, age|
     lowest_key = name
   end
 end
-
-lowest_key
+lowest_value=nil
+  name_hash.collect do |name, age|
+   if lowest_value>age
+  lowest_value=age
+    name
+  end
+end
+lowest_value
 end
